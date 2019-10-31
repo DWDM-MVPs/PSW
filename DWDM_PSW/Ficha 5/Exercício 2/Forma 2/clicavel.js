f7286d4da3ba1d5a19b35c0ce8ff19b228b4c778
Milkenm <miguel.campos.johnny@gmail.com>
2019-10-31 16:03:32 +0000

    F5 E2.2

--- DWDM_PSW/Ficha 5/Exercício 2/Forma 2/clicavel.js
+++ DWDM_PSW/Ficha 5/Exercício 2/Forma 2/clicavel.js
@@ -1,5 +1,10 @@
-﻿var x = false;
+﻿var dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+var index = 0;
 
 function trueFalse() {
-	alert((x = !x) == true ? "Verdadeiro" : "Falso");
+	if (index > 6) index = 0;
+
+	alert(dias[index]);
+
+	index++;
 }
