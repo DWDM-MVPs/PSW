f6a34dd632cf3a8101a3a7020a7a4f9ad0ffeb8d
Milkenm <miguel.campos.johnny@gmail.com>
2019-10-31 15:56:45 +0000

    Update F5 E2.1

--- DWDM_PSW/Ficha 5/Exercício 2/Forma 1/clicavel.js
+++ DWDM_PSW/Ficha 5/Exercício 2/Forma 1/clicavel.js
@@ -1,19 +1,15 @@
 ﻿var dia = 0;
 
 function trueFalse() {
-	var texto;
-
 	if (dia > 6) dia = 0;
 
-	if (dia == 0) texto = "Domingo";
-	else if (dia == 1) texto = "Segunda";
-	else if (dia == 2) texto = "Terça";
-	else if (dia == 3) texto = "Quarta";
-	else if (dia == 4) texto = "Quinta";
-	else if (dia == 5) texto = "Sexta";
-	else if (dia == 6) texto = "Sábado";
+	if (dia == 0) alert("Domingo");
+	else if (dia == 1) alert("Segunda");
+	else if (dia == 2) alert("Terça");
+	else if (dia == 3) alert("Quarta");
+	else if (dia == 4) alert("Quinta");
+	else if (dia == 5) alert("Sexta");
+	else if (dia == 6) alert("Sábado");
 
 	dia++;
-
-	alert(texto);
 }
