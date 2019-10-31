8669d2223fff26296af05422304214de984d8996
Milkenm <miguel.campos.johnny@gmail.com>
2019-10-31 18:23:15 +0000

    F5 E4

---
+++ DWDM_PSW/Ficha 5/Exercício 4/java.js
@@ -0,0 +1,28 @@
+﻿var numerico = document.getElementById("numeric");
+var botao = document.getElementById("button");
+
+
+
+var numero = Math.floor(Math.random() * 10 + 1);
+var opcoes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+
+numerico.onkeyup = function () {
+
+	if (opcoes[numerico.value] != undefined) botao.disabled = false;
+	else botao.disabled = true;
+}
+
+botao.onclick = function () {
+	var guess = document.getElementById("numeric").value;
+
+	numerico.value = '';
+
+	if (guess == numero) {
+		numerico.disabled = botao.disabled = true;
+		alert("Acertou!");
+	}
+	else if (guess > numero) alert("Tenta um número mais baixo.");
+	else alert("Tenta um número mais alto.")
+}
