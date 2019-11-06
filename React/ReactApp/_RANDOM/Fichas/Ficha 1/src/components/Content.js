a252f056a2a7c03ab54368a381055981f43a70a1
Milkenm <miguel.campos.johnny@gmail.com>
2019-11-06 01:20:36 +0000

    Ficha 1

---
+++ React/ReactApp/_RANDOM/Fichas/Ficha 1/src/components/Content.js
@@ -0,0 +1,32 @@
+﻿import React from 'react';
+import '../App.css';
+
+class Content extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Top 3 Herois</h1>
+
+				<div className="Content">
+					<img src="img/superman.jpg" alt="Superman" width="184" height="384" />
+					<div>Superman</div>
+				</div>
+
+				<div className="Content">
+					<img src="img/batman.jpg" alt="Batman" width="184" height="384" />
+					<div>Batman</div>
+				</div>
+
+				<div className="Content">
+					<img src="img/wonderwoman.jpg" alt="Wonder Woman" width="184" height="384" />
+					<div>Wonder Woman</div>
+				</div>
+
+				<br />
+				<img src="img/diagrama.png" className="Diagrama" />
+			</div>
+		);
+	}
+}
+
+export default Content;
---
+++ React/ReactApp/_RANDOM/Fichas/Ficha 1/src/components/Header.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+
+class Header extends React.Component {
+	render() {
+		return (
+			<div className="Header">
+				<h1>League of Heroes</h1>
+				<h3>by Miguel Campos</h3>
+			</div>
+		);
+	}
+}
+
+export default Header;
