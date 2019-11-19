751b11e7ec85796f030a469ffb4055fc18cf5357
Milkenm <miguel.campos.johnny@gmail.com>
2019-11-19 11:42:16 +0000

    Final Firebase

---
+++ React/ReactApp/build/precache-manifest.e07a6f70aba3b020bf8533ceabe3e292.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f80df3755ce686f4c20ee43da2b5850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07ee6ee12d1fc9f65791",
+    "url": "/static/css/main.e5f45b34.chunk.css"
+  },
+  {
+    "revision": "759cfb8050265f427801",
+    "url": "/static/js/2.adb67054.chunk.js"
+  },
+  {
+    "revision": "07ee6ee12d1fc9f65791",
+    "url": "/static/js/main.da1a9092.chunk.js"
+  },
+  {
+    "revision": "863a75be61cb442bf56d",
+    "url": "/static/js/runtime-main.fc69315b.js"
+  }
+]);
